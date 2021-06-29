307626cec40955a01e959e4737c33091859ea221
Dominic Koppatz <dominickoppatz2003@gmail.com>
2021-06-29 21:55:01 +0200

    Fix docs

--- src/pages/index.js
+++ src/pages/index.js
@@ -66,14 +66,6 @@ function Home() {
           <h1 className="hero__title">{siteConfig.title}</h1>
           <p className="hero__subtitle">{siteConfig.tagline}</p>
           <div className={styles.buttons}>
-            // <Link
-            //   className={clsx(
-            //     'button button--outline button--secondary button--lg',
-            //     styles.getStarted,
-            //   )}
-            //   to={useBaseUrl('/')}>
-            //   View Documentation
-            // </Link>
           </div>
         </div>
       </header>
